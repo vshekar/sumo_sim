5b1b5ead5f400e6163020013df07585e2e5901ea
shekar <gv.shekar@gmail.com>
2016-07-25 18:43:11 -0400

    Colored graphs

--- simple/draw_network_graphtools.py
+++ simple/draw_network_graphtools.py
@@ -1,9 +1,28 @@
 from graph_tool.all import *
 import os
 import xml.etree.ElementTree as ET
-
+import colorsys
 
 list_file = os.listdir("./output")
+
+def color_convert(maxd):
+    red = 0.0
+    green = 1.0
+    RGB_list = []
+    while(red < 1.0):
+        red += 2.0/maxd
+        if (red > 1.0):
+            red = 1.0
+        RGB_list.append([red,green,0,1.0])
+        
+    while(green > 0):
+        green -= 2.0/maxd
+        if (green < 0.0):
+            green =0.0
+        RGB_list.append([red,green,0,1.0])
+        
+    
+    return RGB_list
 
 if __name__=='__main__':
     g = Graph()
@@ -12,6 +31,7 @@ if __name__=='__main__':
     pos = {0:(0,0),1:(800,-800),2:(800,800),3:(2000,-800),4:(2000,800),5:(2800,0)}
     vprop_pos = g.new_vertex_property("vector<double>")
     eprop_marker = g.new_edge_property("double")
+    eprop_color = [g.new_edge_property("vector<double>"),g.new_edge_property("vector<double>"),g.new_edge_property("vector<double>")]
     eprop_thickness = [g.new_edge_property("double"),g.new_edge_property("double"),g.new_edge_property("double")]
     
     eprop_text = [g.new_edge_property("string"),g.new_edge_property("string"),g.new_edge_property("string")]
@@ -22,7 +42,7 @@ if __name__=='__main__':
     densities = [t1_density,t2_density,t3_density]
 
     for fn in list_file:
-        if fn == 'edgeData_2to4__500_1000.xml':
+        if fn == 'edgeData.xml':
         #if fn == 'edgeData.xml':
             tree = ET.parse('./output/'+fn)
             root = tree.getroot()
@@ -38,18 +58,30 @@ if __name__=='__main__':
         g.add_vertex()
         vprop_pos[i] = [pos[i][0],pos[i][1]]
     
+    
+    
     for edge in edges:
         e = g.add_edge(edge[0],edge[1])
         for i in range(3):
+            
+            #max_den = densities[i][max(densities[i])]
+            max_den = densities[i][max(densities[i], key=densities[i].get)]
+            print "max_den = " + str(max_den)
+            RGB_list = color_convert(max_den)
+            print len(RGB_list)
             if str(edge[0])+'to'+str(edge[1]) in densities[i].keys():
                 eprop_thickness[i][e] = densities[i][str(edge[0])+'to'+str(edge[1])]
                 eprop_text[i][e] = str(densities[i][str(edge[0])+'to'+str(edge[1])])
+                #eprop_color[i][e] = color_convert(densities[i][str(edge[0])+'to'+str(edge[1])],max_den)
+                print int(densities[i][str(edge[0])+'to'+str(edge[1])])
+                eprop_color[i][e] = RGB_list[int(densities[i][str(edge[0])+'to'+str(edge[1])])-1]
             else:
                 eprop_thickness[i][e] = 1.0
                 eprop_text[i][e] = '0.0'
+                eprop_color[i][e] = RGB_list[0]
     
     for i in range(3):
-        graph_draw(g,vertex_text = g.vertex_index, vertex_font_size=40,vertex_fill_color='white',output_size=(1400,800),output="graph_2to4_t"+str(i+1)+".pdf",pos = vprop_pos,edge_marker_size=40,vertex_size=45,edge_pen_width=3.0,edge_text=eprop_text[i],edge_font_size=45)
+        graph_draw(g,vertex_text = g.vertex_index, vertex_font_size=50,vertex_fill_color='white',output_size=(1300,900),output="graph_t"+str(i+1)+".pdf",pos = vprop_pos,edge_marker_size=40,vertex_size=45,edge_pen_width=eprop_thickness[i]  ,edge_text=eprop_text[i],edge_font_size=45,edge_color=eprop_color[i])
  
 
 
