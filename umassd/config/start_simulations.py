a4251b77ab6ef08e4935eb239a8c449cd1649351
Venkateswaran Shekar <vshekar@umassd.edu>
2016-07-26 20:38:34 -0700

    Fixed teleporting issue

--- umassd/config/start_simulations.py
+++ umassd/config/start_simulations.py
@@ -78,7 +78,7 @@ def generate_additional(edge,G,interval,intervals,suffix):
 def start(G):
     suffix = ""
     #vul_edges = ['4to5','3to5','3to4','1to3','2to4','1to2','0to1','0to2']
-    vul_edges = {'--12814#1':['--12805','--12814#2'],'--12814#2':['-12790','--12814#3'],'--12814#3':['-12811','--12814#4'],'--12814#4':['--12814#5','--12821'],'--12814#6':['--12814#7','-12801#1'],'--12814#7':['-12797','--12814#8'],'--12814#9':['-12804','--12814#10'],'--12814#10':['-12782','--12814#11'],'-12814#13':['-12770','-12814#12'],'-12814#14':['--12786','-12814#13'],'-12814#15':['--12816','-12814#14'],'-12814#16':['-12798','-12814#15']}
+    vul_edges = {'--12814#1':['--12805','--12814#2'],'--12814#2':['-12790','--12814#3'],'--12814#3':['-12811','--12814#4'],'--12814#4':['--12814#5','--12821'],'--12814#6':['--12814#7','-12801#1'],'--12814#7':['-12797','--12814#8'],'--12814#9':['-12804','--12814#10'],'--12814#10':['-12782','--12814#11'],'-12814#13':['-12770','-12814#12'],'-12814#14':['--12786#0','-12814#13'],'-12814#15':['--12816','-12814#14'],'-12814#16':['-12798','-12814#15']}
     #dest_edges = ['4to5','3to5']
     intervals = [(0,3000),(3000,6000),(6000,9000)]
     for edge in vul_edges.items():
@@ -86,10 +86,10 @@ def start(G):
             suffix = '_' + edge[0] + '__' +str(interval[0]) + '_' +str(interval[1])
             generate_config(edge,interval,suffix)
             generate_additional(edge,G,interval,intervals,suffix)
-            print suffix +'\n'
-            sumoProcess = subprocess.Popen(['sumo', "-c", "./config"+suffix+".sumocfg"], stdout=sys.stdout, stderr=sys.stderr)
+            #print suffix +'\n'
+            sumoProcess = subprocess.Popen(['sumo',"--time-to-teleport","-1", "-c", "./config"+suffix+".sumocfg"], stdout=sys.stdout, stderr=sys.stderr)
             sumoProcess.wait()
-            print "\n"
+            #print "\n"
                     
 if __name__=='__main__':
     G = nx.DiGraph()
--- umassd/genTripUmassD.py
+++ umassd/genTripUmassD.py
@@ -14,7 +14,7 @@ def gen_trips():
     curr_car = 0
     st = ""
     while curr_car < tot_cars:
-        st += '<trip id="%d" depart="%.2f" from="%s" to="%s"/>\n' % (curr_car, curr_car*1.0,random.choice(source_list),random.choice(dest_list))
+        st += '<trip id="%d" depart="%.2f" from="%s" to="%s"/>\n' % (curr_car, curr_car*2.0,random.choice(source_list),random.choice(dest_list))
         curr_car += 1
     return st
 	
@@ -27,7 +27,7 @@ def gen_trips_new():
         curr_car += 1
     return st
 
-fl += gen_trips_new()
+fl += gen_trips()
 fl += '</trips>'
 
 f = open('./trips/trip.xml','w')
