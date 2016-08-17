aef7a6a9d77996b4eabd58ac1cfd51683d2d1223
Venkateswaran Shekar <vshekar@umassd.edu>
2016-08-17 13:47:12 -0700

    Update

--- nyc/config/run_simulations.py
+++ nyc/config/run_simulations.py
@@ -1,3 +1,5 @@
+import xml.etree.ElementTree as ET
+
 
 def chunk_gen(lst, n):
     n = len(lst)/n
@@ -7,13 +9,25 @@ def chunk_gen(lst, n):
 def write_zone(lst,i):
     f = open('../network/zone'+str(i)+'.txt','w')
     for link in lst:
-        f.write('edge:'+link+'\n')
+        f.write('junction:'+link+'\n')
     f.close()
 
+def get_sources2():
+    fn = '../network/nyc.net.xml'
+    tree = ET.parse(fn)
+    root = tree.getroot()
+    prev_id = "None"
+    sources = []
+    for el in root:
+        if el.tag == 'junction' and 'function' not in el.attrib.keys() and 'shape' in el.attrib.keys():
+            sources.append(el.attrib['id'])
+    link_lists = chunk_gen(sources, 20)
+    for i in range(20):
+        write_zone(next(link_lists),i)
 
 def get_sources():
     sources = []
-    f = open('../network/Manhattan.txt','r')
+    f = open('../network/Test_manhattan.txt','r')
     for line in f.readlines():
         if 'edge' in line:
             sources.append(line[5:])
@@ -25,4 +39,4 @@ def get_sources():
         
 
 
-get_sources()
+get_sources2()
